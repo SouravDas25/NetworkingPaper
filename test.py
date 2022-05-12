5fcc0710f30d27400ec99495718850d23cd52c59
SilentSamurai <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>
2022-05-12 22:34:24 +0530

    ads

--- test.py
+++ test.py
@@ -1,72 +1,38 @@
-import heapq
+def findMedianSortedArrays(num1, num2) -> float:
+    if len(num2) < len(num1):
+        num1, num2 = num2, num1
+
+    x = len(num1)
+    y = len(num2)
+
+    l = 0
+    h = x
+
+    midOfMerged = (x + y + 1) // 2
+
+    while l <= h:
+        mid = (h + l) // 2
+        print(l, mid, h, midOfMerged)
+
+        leftUp = num1[mid - 1] if mid > 0 else float("-inf")
+        leftDown = num2[midOfMerged - mid - 1] if midOfMerged - mid > 0 else float("-inf")
+
+        rightUp = num1[mid] if mid < x else float("inf")
+        rightDown = num2[midOfMerged - mid] if midOfMerged - mid < y else float("inf")
+
+        if leftUp < rightDown and leftDown < rightUp:
+            if (x + y) % 2 == 0:
+                return (max(leftUp, leftDown) + min(rightDown, rightUp)) / 2
+            else:
+                return max(leftUp, leftDown)
+        elif leftUp > leftDown:
+            h = mid - 1
+        else:
+            l = mid + 1
+    return 0
 
 
-class KSortTuple():
+nums1 = [1]
+nums2 = [1]
 
-    def __init__(self, n, ai):
-        self.n = n
-        self.ai = ai
-
-    def __lt__(self, kst):
-        return self.n.__lt__(kst.n)
-
-    def __le__(self, kst):
-        return self.n.__le__(kst.n)
-
-    def __eq__(self, kst):
-        return self.n.__eq__(kst.n)
-
-    def __ne__(self, kst):
-        return self.n.__ne__(kst.n)
-
-    def __gt__(self, kst):
-        return self.n.__gt__(kst.n)
-
-    def __ge__(self, kst):
-        return self.n.__ge__(kst.n)
-
-    def __str__(self) -> str:
-        return str(self.n)
-
-    def __repr__(self) -> str:
-        return str(self.n)
-
-
-def mergeKsortedList(arrays):
-    merged = []
-    minHeap = []
-
-    pointers = []
-
-    tl = 0
-    for i in range(len(arrays)):
-        heapq.heappush(minHeap, KSortTuple(arrays[i][0], i))
-        tl += len(arrays[i])
-        pointers.append(0)
-
-    print(minHeap)
-
-    while len(merged) < tl:
-        minItem = heapq.heappop(minHeap)
-        merged.append(minItem.n)
-
-        arrayIndex = minItem.ai
-
-        pointers[arrayIndex] += 1
-        valIndex = pointers[arrayIndex]
-
-        print(arrayIndex, valIndex, minItem, minHeap, merged)
-
-        if valIndex < len(arrays[arrayIndex]) :
-            heapq.heappush(minHeap, KSortTuple(arrays[arrayIndex][valIndex], arrayIndex))
-
-    return merged
-
-
-arrays = [
-    [1, 3, 5, 7],
-    [2, 4, 6, 8],
-    [0, 9, 10, 11]
-]
-
-mergeKsortedList(arrays)
+print(findMedianSortedArrays(nums1, nums2))
