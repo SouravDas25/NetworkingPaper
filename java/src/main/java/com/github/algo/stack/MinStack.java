2d0f60e992ff50fb78d34095f2ddad5336333287
SilentSamurai <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>
2022-06-24 00:08:07 +0530

    java algo

--- java/src/main/java/com/github/algo/stack/MinStack.java
+++ java/src/main/java/com/github/algo/stack/MinStack.java
@@ -1,16 +1,31 @@
+package com.github.algo.stack;
 
 
-
+import java.util.Objects;
+import java.util.Stack;
 
 public class MinStack {
 
+    public Stack<Integer> stack = new Stack<>();
+    public Stack<Integer> minStack = new Stack<>();
 
-
-    public static void main(String[] args) {
-        System.out.println("Apple");
+    public void push(Integer value) {
+        if (this.stack.empty() || this.stack.peek() > value) {
+            this.minStack.push(value);
+        }
+        this.stack.push(value);
     }
 
+    public Integer pop() {
+        if (Objects.equals(this.stack.peek(), this.minStack.peek())) {
+            this.minStack.pop();
+        }
+        return this.stack.pop();
+    }
 
+    public Integer min() {
+        return this.minStack.peek();
+    }
 }
 
 
---
+++ java/src/test/java/com/gihub/algo/TestMinStack.java
@@ -0,0 +1,45 @@
+package com.gihub.algo;
+
+
+import com.github.algo.stack.MinStack;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TestMinStack {
+
+    @Test
+    public void minStackTest() {
+        MinStack minStack = new MinStack();
+        minStack.push(3);
+        assertThat(minStack.min()).isEqualTo(3);
+        minStack.push(2);
+        assertThat(minStack.min()).isEqualTo(2);
+        minStack.push(4);
+        assertThat(minStack.min()).isEqualTo(2);
+        minStack.push(5);
+        assertThat(minStack.min()).isEqualTo(2);
+        minStack.push(1);
+        assertThat(minStack.min()).isEqualTo(1);
+
+
+        Integer value = minStack.pop();
+        assertThat(value).isEqualTo(1);
+        assertThat(minStack.min()).isEqualTo(2);
+
+        value = minStack.pop();
+        assertThat(value).isEqualTo(5);
+        assertThat(minStack.min()).isEqualTo(2);
+
+        value = minStack.pop();
+        assertThat(value).isEqualTo(4);
+        assertThat(minStack.min()).isEqualTo(2);
+
+        value = minStack.pop();
+        assertThat(value).isEqualTo(2);
+        assertThat(minStack.min()).isEqualTo(3);
+
+        value = minStack.pop();
+        assertThat(value).isEqualTo(3);
+    }
+}
