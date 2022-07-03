c40885fc3c2258b0a5efde6be675e1a0c7f37f7e
SilentSamurai <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>
2022-07-04 01:01:37 +0530

    java algo

--- java/src/main/java/com/github/algo/arrays/GasStation.java
+++ java/src/main/java/com/github/algo/arrays/GasStation.java
@@ -3,7 +3,19 @@ package com.github.algo.arrays;
 public class GasStation {
 
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        return 0;
+        int net = 0, sum = 0;
+        int changeI = -1;
+        for (int i = 0; i < gas.length; i++) {
+            int d = gas[i] - cost[i];
+            net += d;
+            sum += d;
+            if (sum < 0) {
+                sum = 0;
+                changeI = i + 1;
+            }
+
+        }
+        return net < 0 ? -1 : changeI;
     }
 
 }
