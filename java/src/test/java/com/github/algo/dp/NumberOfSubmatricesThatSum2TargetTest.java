7cafa5f40f75f3dd7381a03dd8c5205fcd010974
Sourav Das <sourav.das01@sap.com>
2022-07-21 11:49:48 +0530

    graph

--- java/src/test/java/com/github/algo/dp/NumberOfSubmatricesThatSum2TargetTest.java
+++ java/src/test/java/com/github/algo/dp/NumberOfSubmatricesThatSum2TargetTest.java
@@ -2,6 +2,9 @@ package com.github.algo.dp;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 class NumberOfSubmatricesThatSum2TargetTest {
 
     @Test
@@ -11,4 +14,20 @@ class NumberOfSubmatricesThatSum2TargetTest {
         int i = numberOfSubmatricesThatSum2Target.numMatrixSumTarget(matrix, 0);
         System.out.println(i);
     }
+
+    @Test
+    void kjabsd() {
+        String line = "1,2,3,4,5:2";
+        String[] split = line.split(":");
+        String[] split1 = split[0].split(",");
+        List<Integer> collect = Arrays.stream(split1).map(Integer::parseInt).collect(Collectors.toList());
+        Collections.reverse(collect.subList(0, 2));
+        Iterator<Integer> iterator = collect.iterator();
+        while (iterator.hasNext()) {
+            Integer next = iterator.next();
+            System.out.print(next);
+            if(iterator.hasNext()) System.out.print(",");
+        }
+    }
+
 }
