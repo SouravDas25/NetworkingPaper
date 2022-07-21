48fc8e0dfa4f4371790928a0dcfe1724198c298f
Sourav Das <sourav.das01@sap.com>
2022-07-21 15:07:06 +0530

    graph

---
+++ java/src/test/java/com/github/algo/TestClassTest.java
@@ -0,0 +1,14 @@
+package com.github.algo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestClassTest {
+
+    @Test
+    public void test() {
+
+    }
+
+}
