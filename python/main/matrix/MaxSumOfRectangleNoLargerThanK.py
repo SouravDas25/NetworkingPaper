95444ffa711e86a423d8b05a04996cbd8c3a51ed
SilentSamurai <souravbumbadas25@gmail.com>
2022-08-27 20:55:07 +0530

    top coding challenge

---
+++ python/main/matrix/MaxSumOfRectangleNoLargerThanK.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+# problem: https://leetcode.com/problems/max-sum-of-rectangle-no-larger-than-k/
+class Solution:
+    def maxSumSubmatrix(self, matrix: List[List[int]], k: int) -> int:
+
+
+        for i in range
+
+
+if __name__ == "__main__":
+    s = Solution()
+    r = s.maxSumSubmatrix([[1, 0, 1], [0, -2, 3]], 2)
+    print(r)
+
