ce4c8202080c1569fb5f8186d9080d9a83ba7c89
SilentSamurai <souravbumbadas25@gmail.com>
2022-08-28 18:47:31 +0530

    top coding challenge

---
+++ python/main/math/IntegerToRoman.py
@@ -0,0 +1,10 @@
+FACTORS = [1000, 500, 100, 50, 10, 5, 1]
+
+
+class Solution:
+
+    def intToRoman(self, num: int) -> str:
+        while num > 0:
+            for factor in FACTORS:
+
+        pass
