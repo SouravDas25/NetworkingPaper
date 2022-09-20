4def3c08fc2a10d7e4ca40d06321a79d07265c94
Sourav Das <sourav.das01@sap.com>
2022-09-20 17:13:24 +0530

    top interview questions

--- python/main/string/MinimumRemoveMakeValidParentheses.py
+++ python/main/string/MinimumRemoveMakeValidParentheses.py
@@ -4,14 +4,26 @@ from collections import deque
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
         stack = deque()
-
+        removeIndex = set()
         for i in range(len(s)):
             if s[i] == "(":
-                stack.append(s[i])
+                stack.append(i)
+            elif s[i] == ")":
+                if stack:
+                    stack.pop()
+                else:
+                    removeIndex.add(i)
+        newString = ""
+        removeIndex.update(stack)
+        for i in range(len(s)):
+            if i in removeIndex:
+                continue
+            newString += s[i]
+        return newString
         pass
 
 
 if __name__ == "__main__":
     solution = Solution()
-    output = solution.minRemoveToMakeValid("lee(t(c)o)de)")
+    output = solution.minRemoveToMakeValid("a(cd(ef")
     print(output)
