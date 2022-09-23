07d873f65c5a8ee840a6dd6ea14e1b96468b260c
SilentSamurai <souravbumbadas25@gmail.com>
2022-09-24 01:34:48 +0530

    top coding challenge

--- python/main/heap/HeapSort.py
+++ python/main/heap/HeapSort.py
@@ -3,60 +3,58 @@ from typing import List
 
 class MinHeap:
 
-    def __init__(self, randomAccess):
-        self.heap: List = randomAccess
-        self.heapify()
-
-    def __insert(self, size):
+    @classmethod
+    def __insert(cls, heap, size):
         i = size
         while i > 0:
             parent = (i - 1) // 2
-            if self.heap[parent] > self.heap[i]:
-                self.heap[parent], self.heap[i] = self.heap[i], self.heap[parent]
+            if heap[parent] > heap[i]:
+                heap[parent], heap[i] = heap[i], heap[parent]
             i = parent
 
-    def __pop(self, size):
-        self.heap[0], self.heap[size] = self.heap[size], self.heap[0]
+    @classmethod
+    def __pop(cls, heap, size):
+        heap[0], heap[size] = heap[size], heap[0]
         i = 0
         left = i * 2 + 1
         right = i * 2 + 2
         while right < size or left < size:
-            if (not right < size) or self.heap[left] <= self.heap[right]:
-                if self.heap[left] < self.heap[i]:
-                    self.heap[left], self.heap[i] = self.heap[i], self.heap[left]
+            if (not right < size) or heap[left] <= heap[right]:
+                if heap[left] < heap[i]:
+                    heap[left], heap[i] = heap[i], heap[left]
                     i = left
                 else:
                     break
             else:
-                if self.heap[right] < self.heap[i]:
-                    self.heap[right], self.heap[i] = self.heap[i], self.heap[right]
+                if heap[right] < heap[i]:
+                    heap[right], heap[i] = heap[i], heap[right]
                     i = right
                 else:
                     break
             left = i * 2 + 1
             right = i * 2 + 2
 
-    def heapify(self):
-        for i in range(len(self.heap)):
-            self.__insert(i)
+    @classmethod
+    def heapify(cls, iterable: List):
+        for i in range(len(iterable)):
+            cls.__insert(iterable, i)
 
-    def insert(self, key):
-        self.heap.append(key)
-        self.__insert(len(self.heap) - 1)
+    @classmethod
+    def insert(cls, heap, value):
+        value.append(value)
+        cls.__insert(heap, len(heap) - 1)
 
-    def pop(self):
-        self.__pop(len(self.heap) - 1)
-        return self.heap.pop()
+    @classmethod
+    def pop(cls, heap):
+        cls.__pop(heap, len(heap) - 1)
+        return heap.pop()
 
-    def __len__(self):
-        return self.heap.__len__()
-
-    @staticmethod
-    def heapsort(iterable: List):
-        minHeap = MinHeap(iterable)
+    @classmethod
+    def heapsort(cls, iterable: List):
+        cls.heapify(iterable)
         # print(minHeap.heap)
-        for i in range(len(minHeap) - 1, -1, -1):
-            minHeap.__pop(i)
+        for i in range(len(iterable) - 1, -1, -1):
+            cls.__pop(iterable, i)
             # print(minHeap.heap, minHeap.heap[i], i)
         iterable.reverse()
 
