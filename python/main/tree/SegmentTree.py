8ef67f2dd8e8665c2fd942c263a9e9713b90a846
SilentSamurai <souravbumbadas25@gmail.com>
2022-10-09 17:56:29 +0530

    top coding challenge

--- python/main/tree/SegmentTree.py
+++ python/main/tree/SegmentTree.py
@@ -1,25 +1,39 @@
+from typing import List, Dict
+
+
+def getCapacity(size: int) -> int:
+    i = 0
+    while 2 ** i < size:
+        i += 1
+    return (2 ** i) * 2 - 1
+
+
+SUM = 0
+MIN = 1
+MAX = 2
+
+
 class SegmentTree:
 
     def __init__(self, elements):
-        self.tree = [0] * (self.getCapacity(len(elements)))
+        self.capacity = getCapacity(len(elements))
+        self.tree: List[Dict] = [None] * self.capacity
         self.size = len(elements)
         self.elements = elements
+        for i in range(self.capacity):
+            self.tree[i] = [0, float("inf"), float("-inf")]
         for i in range(self.size):
-            self.update(i, 2 * elements[i])
+            self.__update(i, elements[i], False)
 
-    def getCapacity(self, size):
-        i = 0
-        while 2 ** i < size:
-            i += 1
-        return (2 ** i) * 2 - 1
-
-    def update(self, index, value):
+    def __update(self, index, value, withDiff=True):
         node = 0
-        diff = value - self.elements[index]
         low = 0
+        diff = value
+        if withDiff:
+            diff = value - self.elements[index]
         high = self.size - 1
         while node < len(self.tree) and low <= high:
-            self.tree[node] += diff
+            self.tree[node][SUM] += diff
             mid = low + (high - low) // 2
             if index <= mid:
                 high = mid
@@ -27,18 +41,62 @@ class SegmentTree:
             else:
                 low = mid + 1
                 node = node * 2 + 2
+
+        node = (node - 1) // 2
+        self.tree[node][MIN] = value
+        self.tree[node][MAX] = value
+        while node > 0:
+            node = (node - 1) // 2
+            left = node * 2 + 1
+            right = node * 2 + 2
+            self.tree[node][MIN] = min(self.tree[left][MIN], self.tree[right][MIN])
+            self.tree[node][MAX] = max(self.tree[left][MAX], self.tree[right][MAX])
         pass
 
-    def sum(self, lo, hi, low=None, high=None, node=0):
-        if low is None:
-            low = 0
-        if high is None:
-            high = self.size - 1
+    def update(self, index, value):
+        self.__update(index, value)
 
+    def __reduce(self, lo, hi, low, high, node, reductionLambda):
         mid = low + (high - low) // 2
+        if hi < low:
+            return reductionLambda["UOOB"]()
+        if lo > high:
+            return reductionLambda["LOOB"]()
         if low == lo and high == hi:
-            return self.tree[node]
-        return self.sum(lo, mid, low, mid, node * 2 + 1) + self.sum(mid + 1, hi, mid + 1, high, node * 2 + 2)
+            return reductionLambda["EXACT"](self.tree[node])
+        left = self.__reduce(lo, min(mid, hi), low, mid, node * 2 + 1, reductionLambda)
+        right = self.__reduce(max(mid + 1, lo), hi, mid + 1, high, node * 2 + 2, reductionLambda)
+        return reductionLambda["REDUCE"](left, right)
+
+    def reduce(self, lo, hi, reductionLambda):
+        return self.__reduce(lo, hi, 0, self.size - 1, 0, reductionLambda)
+
+    def sum(self, lo, hi):
+        reductionLambda = {
+            "REDUCE": lambda a, b: a + b,
+            "UOOB": lambda: 0,
+            "LOOB": lambda: 0,
+            "EXACT": lambda value: value[SUM]
+        }
+        return self.reduce(lo, hi, reductionLambda)
+
+    def min(self, lo, hi):
+        reductionLambda = {
+            "REDUCE": lambda a, b: min(a, b),
+            "UOOB": lambda: float("inf"),
+            "LOOB": lambda: float("inf"),
+            "EXACT": lambda value: value[MIN]
+        }
+        return self.reduce(lo, hi, reductionLambda)
+
+    def max(self, lo, hi):
+        reductionLambda = {
+            "REDUCE": lambda a, b: max(a, b),
+            "UOOB": lambda: float("-inf"),
+            "LOOB": lambda: float("-inf"),
+            "EXACT": lambda value: value[MAX]
+        }
+        return self.reduce(lo, hi, reductionLambda)
 
     def print(self, prefix="", node=0, isLeft=False, low=None, high=None):
         if low is None:
@@ -46,14 +104,15 @@ class SegmentTree:
         if high is None:
             high = self.size - 1
         if node < len(self.tree):
-            print(prefix + ("L-- " if isLeft else "R-- ") + str(self.tree[node]), "({} - {})".format(low, high))
+            print(prefix + ("L-- " if isLeft else "R-- "), "({} - {})".format(low, high), str(self.tree[node]))
             mid = low + (high - low) // 2
             self.print(prefix + ("|   " if isLeft else "    "), node * 2 + 1, True, low, mid)
             self.print(prefix + ("|   " if isLeft else "    "), node * 2 + 2, False, mid + 1, high)
 
 
 if __name__ == "__main__":
-    segmentTree = SegmentTree([1, 2, 3, 4, 5, 6, 7])
-    print(segmentTree.sum(0, 5))
+    segmentTree = SegmentTree([1, 2, 3, 4, 5, 6, 7, 8])
+    # segmentTree.update(3, 9)
+    print(segmentTree.max(3, 5))
     print(segmentTree.tree)
     segmentTree.print()
