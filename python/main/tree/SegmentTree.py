8da7ca63baa109de9a4941a82a5d441b004b00cd
Sourav Das <sourav.das01@sap.com>
2022-10-09 15:51:53 +0530

    top interview questions

--- python/main/tree/SegmentTree.py
+++ python/main/tree/SegmentTree.py
@@ -1,32 +1,59 @@
 class SegmentTree:
 
     def __init__(self, elements):
-        self.tree = [0] * (2 * len(elements) - 1)
+        self.tree = [0] * (self.getCapacity(len(elements)))
         self.size = len(elements)
         self.elements = elements
         for i in range(self.size):
             self.update(i, 2 * elements[i])
 
+    def getCapacity(self, size):
+        i = 0
+        while 2 ** i < size:
+            i += 1
+        return (2 ** i) * 2 - 1
+
     def update(self, index, value):
         node = 0
         diff = value - self.elements[index]
-        l = 0
-        r = self.size - 1
-        while l <= r and node < self.size:
+        low = 0
+        high = self.size - 1
+        while node < len(self.tree) and low <= high:
             self.tree[node] += diff
-            mid = l + (r - l) // 2
+            mid = low + (high - low) // 2
             if index <= mid:
-                r = mid
+                high = mid
                 node = node * 2 + 1
             else:
-                l = mid + 1
+                low = mid + 1
                 node = node * 2 + 2
         pass
 
-    def query(self, lo, hi, operation):
-        pass
+    def sum(self, lo, hi, low=None, high=None, node=0):
+        if low is None:
+            low = 0
+        if high is None:
+            high = self.size - 1
+
+        mid = low + (high - low) // 2
+        if low == lo and high == hi:
+            return self.tree[node]
+        return self.sum(lo, mid, low, mid, node * 2 + 1) + self.sum(mid + 1, hi, mid + 1, high, node * 2 + 2)
+
+    def print(self, prefix="", node=0, isLeft=False, low=None, high=None):
+        if low is None:
+            low = 0
+        if high is None:
+            high = self.size - 1
+        if node < len(self.tree):
+            print(prefix + ("L-- " if isLeft else "R-- ") + str(self.tree[node]), "({} - {})".format(low, high))
+            mid = low + (high - low) // 2
+            self.print(prefix + ("|   " if isLeft else "    "), node * 2 + 1, True, low, mid)
+            self.print(prefix + ("|   " if isLeft else "    "), node * 2 + 2, False, mid + 1, high)
 
 
 if __name__ == "__main__":
-    segmentTree = SegmentTree([1, 2])
+    segmentTree = SegmentTree([1, 2, 3, 4, 5, 6, 7])
+    print(segmentTree.sum(0, 5))
     print(segmentTree.tree)
+    segmentTree.print()
