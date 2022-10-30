5366bb0caeed1aa85eba2c741c15fc12dd3acd86
SilentSamurai <souravbumbadas25@gmail.com>
2022-10-30 22:00:28 +0530

    top coding challenge

---
+++ python/main/dp/LongestArithmeticSubsequenceOfGivenDifference.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def longestSubsequence(self, arr: List[int], difference: int) -> int:
+        pass
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    output = solution.longestSubsequence([[1, 1], [2, 3], [2, 3], [1, 1], [1, 1], [1, 1], [1, 2]], 4)
+    print(output)
